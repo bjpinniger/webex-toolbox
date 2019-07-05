c5a1da2c3fdbb5da8459466c6fb764cab61108a6
Brendon Pinniger <bpinniger@gmail.com>
2019-07-05 20:04:07 +1000

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,6 @@ import os
 
 class Config(object):
     MONGO_URI = os.environ.get('MONGO_URI')
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_ECHO = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
     clientID = os.environ.get('CLIENT_ID')
     secretID = os.environ.get('SECRET_ID')
